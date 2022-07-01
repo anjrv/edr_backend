91247cb17b10c6ea0a31d0461b2051c9d638bbde
Anna Jaerving <AnnaJrving@gmail.com>
2022-07-01 15:23:43 +0000

    day and session listing, session fix required on subscriber for unique identification

---
+++ src/api/days.js
@@ -0,0 +1,58 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+import { logger } from '../utils/logger.js';
+
+dotenv.config();
+
+const { DATABASE_URL: db = 'mongodb://127.0.0.1:27017' } = process.env;
+
+const mongoClient = new MongoClient(db);
+
+function validateDate(date) {
+  return isNaN(date) && !isNaN(Date.parse(date));
+}
+
+export async function listDay(req, res) {
+  const { dayId: id } = req.params;
+
+  // Malformed, dont start a connection
+  if (!validateDate(id)) return res.status(400).end();
+
+  const connection = await mongoClient.connect();
+  const db = connection.db(id);
+
+  const results = {};
+
+  results.sessions = await db
+    .collection('sessions')
+    .find({})
+    .project({ _id: 0 })
+    .toArray();
+
+  results.anomalies = await db
+    .collection('anomalies')
+    .find({})
+    .project({ alt: 1, lat: 1, lon: 1, edr: 1, ms: 1, session: 1, _id: 0 })
+    .toArray();
+
+  await mongoClient.close();
+
+  return res.json(results);
+}
+
+export async function listDays(_req, res) {
+  const connection = await mongoClient.connect();
+  const dbs = await connection.db('api').admin().listDatabases();
+  const days = [];
+
+  dbs.databases.forEach((db) => {
+    if (validateDate(db.name)) days.push(db.name);
+  });
+
+  await mongoClient.close();
+
+  if (!days) return res.status(404).json(null);
+
+  return res.json(days);
+}
--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,9 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 
 import { readFile } from '../utils/fileSystem.js';
+import { catchErrors } from '../utils/catchErrors.js'
+import { validationCheck } from '../validators/helpers.js';
+import { listDay, listDays } from './days.js';
 
 // TODO: Import routes and validators
 
@@ -16,3 +19,15 @@ router.get('/', async (_req, res) => {
   const indexJson = await readFile(join(path, './index.json'));
   res.json(JSON.parse(indexJson));
 });
+
+router.get(
+  '/days',
+  validationCheck,
+  catchErrors(listDays),
+);
+
+router.get(
+  '/days/:dayId',
+  validationCheck,
+  catchErrors(listDay),
+);
