013b52845247fdb976c7b3c38f1ceaf60d024376
Anna Jaerving <AnnaJrving@gmail.com>
2022-07-01 20:17:07 +0000

    ensure connection gets closed

--- src/api/days.js
+++ src/api/days.js
@@ -19,54 +19,59 @@ export async function listDay(req, res) {
   // Malformed, dont start a connection
   if (!validateDate(id)) return res.status(400).end();
 
-  const connection = await mongoClient.connect();
-  const db = connection.db(id);
-
   const results = {};
 
-  results.sessions = await db
-    .collection('sessions')
-    .find({})
-    .project({ _id: 0 })
-    .toArray();
+  try {
+    const connection = await mongoClient.connect();
+    const db = connection.db(id);
 
-  results.anomalies = await db
-    .collection('anomalies')
-    .aggregate([
-      {
-        $group: {
-          _id: '$session',
-          measurements: {
-            $push: {
-              alt: '$alt',
-              lat: '$lat',
-              lon: '$lon',
-              edr: '$edr',
-              ms: '$ms',
+    results.sessions = await db
+      .collection('sessions')
+      .find({})
+      .project({ _id: 0 })
+      .toArray();
+
+    results.anomalies = await db
+      .collection('anomalies')
+      .aggregate([
+        {
+          $group: {
+            _id: '$session',
+            measurements: {
+              $push: {
+                alt: '$alt',
+                lat: '$lat',
+                lon: '$lon',
+                edr: '$edr',
+                ms: '$ms',
+              },
             },
           },
         },
-      },
-      { $set: { session: '$_id' } },
-      { $unset: '_id' },
-    ])
-    .toArray();
-
-  await mongoClient.close();
+        { $set: { session: '$_id' } },
+        { $unset: '_id' },
+      ])
+      .toArray();
+  } finally {
+    await mongoClient.close();
+  }
 
   return res.json(results);
 }
 
 export async function listDays(_req, res) {
-  const connection = await mongoClient.connect();
-  const dbs = await connection.db('api').admin().listDatabases();
   const days = [];
 
-  dbs.databases.forEach((db) => {
-    if (validateDate(db.name)) days.push(db.name);
-  });
+  try {
+    const connection = await mongoClient.connect();
+    const dbs = await connection.db('api').admin().listDatabases();
 
-  await mongoClient.close();
+    dbs.databases.forEach((db) => {
+      if (validateDate(db.name)) days.push(db.name);
+    });
+  } finally {
+    await mongoClient.close();
+  }
 
   if (!days) return res.status(404).json(null);
 
